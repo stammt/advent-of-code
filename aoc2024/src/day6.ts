b9317bd2cddd7b5f7575e46cae154cc9b6adbac3
Tom Stamm <stammt@gmail.com>
2024-12-09 07:25:33 -0700

    Day 6

---
+++ aoc2024/src/day6.ts
@@ -0,0 +1,89 @@
+import { readInput } from "./utils/file-utils";
+import { Point, CardinalDirection, parsePoint } from "./utils/point";
+import { isOnTheGrid } from "./utils/char-grid";
+
+const lines = readInput("day6", false);
+
+function getStart(): Point | undefined {
+  for (let y = 0; y < lines.length; y++) {
+    const x = lines[y].indexOf("^");
+    if (x > 0) {
+      return new Point(x, y);
+    }
+  }
+  return undefined;
+}
+
+function getPointsOnPath(
+  start: Point,
+  obstacle?: Point
+): { points: Set<string>; loop: boolean } {
+  let p = start;
+  let dir = CardinalDirection.N;
+  const positions = new Set<string>();
+  const path = new Array<string>();
+  const posdirs = new Set<string>();
+  let loop = false;
+  while (true) {
+    positions.add(p.toString());
+    path.push(p.toString());
+    posdirs.add(`${p.toString()}-${dir}`);
+    const nextPoint = p.step(dir);
+    if (!isOnTheGrid(nextPoint, lines)) {
+      // console.log(
+      //   `guard left the grid with ${obstacle}, path=${path.join(" ")}`
+      // );
+      break;
+    }
+
+    // It's a loop if we get back to a point and direction that we've already seen.
+    // inefficient but it works :-/
+    if (posdirs.has(`${nextPoint.toString()}-${dir}`)) {
+      // console.log(
+      //   `guard looped with ${obstacle}, path=${path.join(" ")}, ${p} and ${nextPoint}`
+      // );
+      loop = true;
+      break;
+    }
+
+    const c = lines[nextPoint.y][nextPoint.x];
+    if (c === "#" || nextPoint.equals(obstacle)) {
+      // Don't move, just rotate
+      if (dir === CardinalDirection.N) dir = CardinalDirection.E;
+      else if (dir === CardinalDirection.E) dir = CardinalDirection.S;
+      else if (dir === CardinalDirection.S) dir = CardinalDirection.W;
+      else if (dir === CardinalDirection.W) dir = CardinalDirection.N;
+    } else {
+      p = nextPoint;
+    }
+  }
+  return { points: positions, loop: loop };
+}
+
+function part1() {
+  const start = getStart();
+  const { points } = getPointsOnPath(start!);
+  console.log(`positions size: ${points.size} `);
+}
+
+function part2() {
+  const start = getStart()!;
+  // const { points: basePoints } = getPointsOnPath(start);
+
+  let count = 0;
+  for (let y = 0; y < lines.length; y++) {
+    for (let x = 0; x < lines[y].length; x++) {
+      const p = new Point(x, y);
+      if (!p.equals(start)) {
+        const { loop } = getPointsOnPath(start, p);
+        if (loop) {
+          count++;
+        }
+      }
+    }
+  }
+
+  console.log(`number of loops: ${count} `);
+}
+
+part2();
--- aoc2024/src/utils/point.ts
+++ aoc2024/src/utils/point.ts
@@ -1,39 +1,75 @@
 export enum CardinalDirection {
-    N = 0,
-    NE,
-    E,
-    SE,
-    S,
-    SW,
-    W,
-    NW
+  N = 0,
+  NE,
+  E,
+  SE,
+  S,
+  SW,
+  W,
+  NW,
 }
-export const CardinalDirections = [CardinalDirection.N, CardinalDirection.NE, CardinalDirection.E, CardinalDirection.SE,
-    CardinalDirection.S, CardinalDirection.SW, CardinalDirection.W, CardinalDirection.NW
-]
+export const CardinalDirections = [
+  CardinalDirection.N,
+  CardinalDirection.NE,
+  CardinalDirection.E,
+  CardinalDirection.SE,
+  CardinalDirection.S,
+  CardinalDirection.SW,
+  CardinalDirection.W,
+  CardinalDirection.NW,
+];
 
 export class Point {
-    readonly x: number;
-    readonly y: number;
+  readonly x: number;
+  readonly y: number;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 
-    step(direction: CardinalDirection) : Point {
-        var nextX = this.x;
-        var nextY = this.y;
-        if (direction == CardinalDirection.N || direction == CardinalDirection.NE || direction == CardinalDirection.NW) {
-            nextY -= 1;
-        } else if (direction == CardinalDirection.S || direction == CardinalDirection.SE || direction == CardinalDirection.SW) {
-            nextY += 1;
-        }
-        if (direction == CardinalDirection.W || direction == CardinalDirection.NW || direction == CardinalDirection.SW) {
-            nextX -= 1;
-        } else if (direction == CardinalDirection.E || direction == CardinalDirection.NE || direction == CardinalDirection.SE) {
-            nextX += 1;
-        }
-        return new Point(nextX, nextY);
+  step(direction: CardinalDirection): Point {
+    let nextX = this.x;
+    let nextY = this.y;
+    if (
+      direction == CardinalDirection.N ||
+      direction == CardinalDirection.NE ||
+      direction == CardinalDirection.NW
+    ) {
+      nextY -= 1;
+    } else if (
+      direction == CardinalDirection.S ||
+      direction == CardinalDirection.SE ||
+      direction == CardinalDirection.SW
+    ) {
+      nextY += 1;
     }
+    if (
+      direction == CardinalDirection.W ||
+      direction == CardinalDirection.NW ||
+      direction == CardinalDirection.SW
+    ) {
+      nextX -= 1;
+    } else if (
+      direction == CardinalDirection.E ||
+      direction == CardinalDirection.NE ||
+      direction == CardinalDirection.SE
+    ) {
+      nextX += 1;
+    }
+    return new Point(nextX, nextY);
+  }
+
+  toString(): string {
+    return `${this.x},${this.y}`;
+  }
+
+  equals(other?: Point): boolean {
+    return other != undefined && this.x === other.x && this.y === other.y;
+  }
+}
+
+export function parsePoint(s: string): Point {
+  const [x, y] = s.split(",").map((e) => parseInt(e));
+  return new Point(x, y);
 }
