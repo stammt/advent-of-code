a1021c8b29ab3978c506f1c934b1773c2badb302
Tom Stamm <stammt@gmail.com>
2024-12-22 11:43:20 -0500

    start day22 part 2

--- aoc2024/src/day22.ts
+++ aoc2024/src/day22.ts
@@ -2,7 +2,7 @@ import { Grid, linesToCharGrid, toNumberGrid } from "./utils/char-grid";
 import { readInput } from "./utils/file-utils";
 import { CardinalDirection, Point } from "./utils/point";
 
-const lines = readInput("day22", false);
+const lines = readInput("day22", true);
 
 function mix(n: bigint, secretNumber: bigint): bigint {
   return n ^ secretNumber;
@@ -15,11 +15,8 @@ function prune(secretNumber: bigint): bigint {
 function nextSecretNumber(secretNumber: bigint): bigint {
   let step1 = secretNumber * 64n;
   step1 = prune(mix(step1, secretNumber));
-
   const step2 = prune(mix(step1 / 32n, step1));
-
   const step3 = prune(mix(step2 * 2048n, step2));
-
   return step3;
 }
 
@@ -37,6 +34,44 @@ function part1() {
   console.log(`sum: ${sum}`);
 }
 
-function part2() {}
+function getPrice(secretNumber: bigint): number {
+  // blah
+  const str = `${secretNumber}`;
+  return parseInt(str.substring(str.length - 1));
+}
+function getPrices(secretNumber: bigint): number[] {
+  const results: number[] = [];
+  results.push(getPrice(secretNumber));
+  const iterations = 2000;
 
-part1();
+  let s = secretNumber;
+  for (let i = 0; i < iterations - 1; i++) {
+    s = nextSecretNumber(s);
+    results.push(getPrice(s));
+  }
+  return results;
+}
+
+function getPriceDiffs(prices: number[]): number[] {
+  const results: number[] = [];
+
+  for (let i = 1; i < prices.length; i++) {
+    results.push(prices[i] - prices[i - 1]);
+  }
+
+  return results;
+}
+
+function part2() {
+  lines.forEach((line) => {
+    const s = BigInt(line);
+    const prices = getPrices(s);
+    const priceDiffs = getPriceDiffs(prices);
+    for (let i = 0; i < prices.length; i++) {
+      const diff = i > 0 ? `${priceDiffs[i - 1]}` : "";
+      console.log(`${prices[i]} : ${diff}`);
+    }
+  });
+}
+
+part2();
