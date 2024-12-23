ece1328990b1401f66efaa428716299594445056
Tom Stamm <stammt@gmail.com>
2024-12-22 22:11:02 -0500

    More trying day21 part 2

--- aoc2024/src/day21.ts
+++ aoc2024/src/day21.ts
@@ -3,7 +3,7 @@ import { readInput } from "./utils/file-utils";
 import { CardinalDirection, Point } from "./utils/point";
 import { allOrderings, stringPermutations } from "./utils/utils";
 
-const lines = readInput("day21", true);
+const lines = readInput("day21", false);
 
 const UP = "^";
 const DOWN = "v";
@@ -143,6 +143,9 @@ function part1() {
 
     let lastPos = startPos;
     let sequenceLength = 0;
+
+    // walk through the sequence one step at a time, finding the shortest moves
+    // for each step.
     for (let i = 0; i < seq.length; i++) {
       // const nextPos = keypad.find(seq[i]);
       console.log(`*** moving from ${lastPos} to ${seq[i]}`);
@@ -155,7 +158,8 @@ function part1() {
       // shortestMoves.forEach((m) => logMoves(m));
 
       // repeat for the next two levels
-      for (let level = 0; level < 2; level++) {
+      for (let level = 0; level < 25; level++) {
+        console.log(`level ${level} with ${shortestMoves.length} moves to try`);
         // dirpad always starts at "A", (2, 0);
         const dirStartPos = new Point(2, 0);
 
@@ -200,42 +204,66 @@ function part2() {
     // Then for each sequence of dirpad presses, expand out to the sequence of presses at the
     // last dirpad.
     const dirpadStartPos = new Point(2, 0);
-    let sequenceLength = 0;
 
+    let shortestSequenceLength = Infinity;
     shortestMoves.forEach((m) => {
+      let sequenceLength = 0;
+      console.log(`looking at ${m}`);
       let dirpadPos = dirpadStartPos;
       for (let i = 0; i < m.length; i++) {
         const dirpadNextPos = dirpad.find(m[i]);
         const cacheKey = `${dirpadPos}:${dirpadNextPos}`;
+        console.log(
+          `Moving dirpad from ${dirpadPos} to ${dirpadNextPos} for ${m[i]}`
+        );
         if (moveCountCache.has(cacheKey)) {
           console.log(
             `cache hit for ${cacheKey}: ${moveCountCache.get(cacheKey)}`
           );
           sequenceLength += moveCountCache.get(cacheKey)!;
         } else {
+          // Build the moves for this level, then for each move see how many moves it expands
+          // to for the next level, and trace/expand that down for all the levels.
+          const levelMoves = buildMoves(m[i], dirpad, dirpadPos);
+          const shortestLevelMoves = Array.from(keepShortest(levelMoves));
+
           // repeat for the next twenty-five levels
-          console.log(
-            `Moving dirpad from ${dirpadPos} to ${dirpadNextPos} for ${m[i]}`
-          );
-          let shortestLevelMoves: string[] = [];
-          for (let level = 0; level < 2; level++) {
-            const levelMoves = buildMoves(m[i], dirpad, dirpadPos);
+
+          let shortestLevelMoves: string[] = [m[i]];
+          // repeat for the next two levels
+          for (let level = 0; level < 25; level++) {
+            const levelMoves = new Set<string>();
+            shortestLevelMoves.forEach((slm) => {
+              const theseMoves = buildMoves(
+                slm,
+                dirpad,
+                level == 0 ? dirpadPos : dirpadStartPos
+              );
+              theseMoves.forEach((l2m) => levelMoves.add(l2m));
+            });
             shortestLevelMoves = Array.from(keepShortest(levelMoves));
+            // console.log(
+            //   `using ${shortestLevelMoves[0].length}: ${shortestLevelMoves[0]}`
+            // );
           }
-          console.log(
-            `using ${shortestLevelMoves[0].length}: ${shortestLevelMoves[0]}`
-          );
+
+          // console.log(
+          //   `using ${shortestLevelMoves[0].length}: ${shortestLevelMoves[0]}`
+          // );
           moveCountCache.set(cacheKey, shortestLevelMoves[0].length);
           sequenceLength += shortestLevelMoves[0].length;
         }
         dirpadPos = dirpadNextPos;
       }
+      if (sequenceLength < shortestSequenceLength) {
+        shortestSequenceLength = sequenceLength;
+      }
     });
 
     const complexity =
-      sequenceLength * parseInt(seq.substring(0, seq.length - 1));
+      shortestSequenceLength * parseInt(seq.substring(0, seq.length - 1));
     console.log(
-      `total length for ${seq}: ${sequenceLength}, complexity ${complexity}`
+      `total length for ${seq}: ${shortestSequenceLength}, complexity ${complexity}`
     );
     complexitySum += complexity;
   });
