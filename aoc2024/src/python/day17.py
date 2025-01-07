d22c7bae0965c075f50530f99cbde18485e78152
Tom Stamm <stammt@gmail.com>
2025-01-07 06:59:15 -0700

    Python day 17 part 2

--- aoc2024/src/python/day17.py
+++ aoc2024/src/python/day17.py
@@ -10,11 +10,11 @@ import sys
 import numpy
 
 
-testInput = r"""Register A: 729
+testInput = r"""Register A: 2024
 Register B: 0
 Register C: 0
 
-Program: 0,1,5,4,3,0"""
+Program: 0,3,5,4,3,0"""
 
 
 input = PuzzleInput('input/day17.txt', testInput)
@@ -69,26 +69,69 @@ ops = {
     7: cdv
 }
 
-def part1():
+def parse_state():
     state = {}
     state[A] = int(lines[0].split(' ')[2])
     state[B] = int(lines[1].split(' ')[2])
     state[C] = int(lines[2].split(' ')[2])
+    return state
+
+def run_program(base_state, program, subA=None) -> list[int]:
+    state = dict(base_state)
+    if subA != None:
+        state[A] = subA
     state[OUTPUT] = []
     state[I] = 0
-
-    program = splitInts(lines[4].split(' ')[1], ',')
-
     while state[I] < len(program) - 1:
         iwas = state[I]
         ops[program[state[I]]](program[state[I]+1], state)
         if state[I] == iwas:
             state[I] = state[I] + 2
+    return state[OUTPUT]
 
-    print(f'Output: {','.join(map(str, state[OUTPUT]))}')
+def part1():
+    state = parse_state()
+    program = splitInts(lines[4].split(' ')[1], ',')
+    output = run_program(state, program)
 
+    print(f'Output: {','.join(map(str, output))}')
+
+# Copied from Norvig's python notebook - I was heading towards this idea but I wouldn't have come up with this
+# solution on my own...
+def quine(state, program) -> Set[int]:
+    """Find the values of register `A` that cause the output of the run to match the program."""
+    As = {0} # Set of candidate partial values for register A
+    for place in reversed(range(len(program))):
+        tail = list(program[place:])
+        candidates = {(A1 * 8) + d for A1 in As for d in range(8)}
+
+        As = {A1 for A1 in candidates if run_program(state, program, subA=A1) == tail}
+    return As
 
 def part2():
-    print(f'nyi')
+    state = parse_state()
+    program = splitInts(lines[4].split(' ')[1], ',')
+    # output = run_program(state, program, subA=0o3)
+    # print(output)
+    As = quine(state, program)
+    print(f'Found {len(As)} candidates, min is {min(As)}')
+
+    # sample A: 117440
+    # printing A % 8 =  0,3, 5,4, 3,0
+    # 0,3  A = A / 8
+    # 5,4  Output A % 8
+    # 3,0  Jump to 0 if A is not zero
+    # 117440, 14680, 1835, 229, 28, 3, 0
+
+    # printing B % 8, A needs to be zero at the end 2,4, 1,1, 7,5, 0,3, 4,3, 1,6, 5,5, 3,0
+    # 2,4:  B = A % 8
+    # 1,1:  B = B ^ 1
+    # 7,5:  C = A / 2 ** B
+    # 0,3:  A = A / 8
+    # 4,3:  B = B ^ C
+    # 1,6:  B = B ^ 6
+    # 5,5:  Output B % 8
+    # 3,0:  Jump to 0 or end if A is 0
+
 
 runIt(part1, part2)
