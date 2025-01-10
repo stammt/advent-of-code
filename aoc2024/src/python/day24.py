fcb01f35f893e4115d362d67191b9166c1388db0
Tom Stamm <stammt@gmail.com>
2025-01-10 14:26:25 -0700

    Python day 24

--- aoc2024/src/python/day24.py
+++ aoc2024/src/python/day24.py
@@ -98,10 +98,133 @@ def part1():
     zd = int(zees, 2)
     print(zd)
 
+def get_swapped_output(a, op, b, gate_lookups, swaps):
+    key = (a, op, b)
+    result = 'NOPE'
+    if key in gate_lookups:
+        result = gate_lookups[key]
+    else:
+        key = (b, op, a)
+        if key in gate_lookups:
+            result = gate_lookups[key]
+    if result != 'NOPE' and result in swaps:
+        print(f'swapping result {result} for {swaps[result]}')
+        result = swaps[result]
+    return result
+
+def diagnose(gates, swaps):
+    # adders https://en.wikipedia.org/wiki/Adder_(electronics)
+    # 1 bit full adder (https://en.wikipedia.org/wiki/File:Full-adder_logic_diagram.svg)
+    # Inputs a,b,c (in)
+    # a XOR b -> w
+    # a AND B -> z
+    # w XOR c -> S
+    # w AND c -> y
+    # y OR z  -> c (out)
+    # Ripple-carry multibit adder chains 1 bit full adders, where c (out) goes to c (in) of the next bit
+
+    inputs = {
+        'a': 0,
+        'b': 1,
+        'c': 1
+    }
+    one_bit_full_adder_gates = {
+        'a': ('x', 'XOR', 'y'),
+        'b': ('x', 'AND', 'y'),
+        'c': ('a', 'AND', 'carry_in'),
+        'S': ('b', 'XOR', 'carry_in'), # output
+        'C': ('c', 'OR', 'b') # carry_out
+    }
+    one_bit_half_adder_gates = {
+        'S': ('a', 'XOR', 'b'),
+        'C': ('a', 'AND', 'b')
+    }
+
+    # z45 is carry bit from last full adder kbm OR spc
+    # z44 output from last full adder hhg XOR hhp
+    # ...
+    # z01 is output from first full adder, mcg is carry in, mcg XOR vcn; x01 XOR y01 -> vcn
+    # z00 is output from first half adder y00 XOR x00, carry  mcg x00 AND y00
+
+    # reverse lookup, we know the gate, what's the output?
+    gate_lookups = {b:a for a,b in gates.items()}
+
+    def get_output(a, op, b):
+        return get_swapped_output(a, op, b, gate_lookups, swaps)
+    
+    # manually verified x00/y00 half adder with output carry mcg
+    # run through the rest until we NOPE out, and print diagnostics
+    # manually figure out the swap and add to map, then run again!
+    carry = 'mcg'
+    for i in range(1,45):
+        carry_in = carry
+        x = 'x{:02d}'.format(i)
+        y = 'y{:02d}'.format(i)
+        z = 'z{:02d}'.format(i)
+
+        # built the adder and see if anything nope's out
+        a = get_output(x, 'XOR', y)
+        b = get_output(x, 'AND', y)
+        c = get_output(a, 'AND', carry_in)
+        output = get_output(a, 'XOR', carry_in)
+        carry = get_output(c, 'OR', b)
+
+        print(f'Gate {i}: output is {output}, carry is {carry}')
+        if output == 'NOPE' or not output.startswith('z'):
+            print(f'a is {a} -> {x} XOR {y}')
+            print(f'b is {b} -> {x} AND {y}')
+            print(f'c is {c} -> {a} AND {carry_in}')
+            print(f'output is {output} -> {a} XOR {carry_in}')
+            print(f'carry is {carry} -> {c} OR {b}')
+            break
+
 
 
 def part2():
-    print('nyi')
+    splits = split_on_empty_lines(lines)
+    inputs = {a:int(b) for a,b in [line.split(': ') for line in splits[0]] }
+    gates = {b:tuple(a.split(' ')) for (a,b) in [line.split(' -> ') for line in splits[1]] }
+
+    swaps = {
+        'shh':'z21',
+        # 'z21':'shh',
+        'vgs':'dtk',
+        # 'dtk':'vgs',
+        'dqr':'z33',
+        # 'z33':'dqr',
+        'pfw':'z39',
+        # 'z39':'pfw'
+    }
+
+    xs = ''.join(map(lambda x: str(inputs[x]), reversed(sorted([k for k in inputs.keys() if k.startswith('x')]))))
+    print(f'X={int(xs, 2)} ({xs})')
+
+    ys = ''.join(map(lambda x: str(inputs[x]), reversed(sorted([k for k in inputs.keys() if k.startswith('y')]))))
+    print(f'Y={int(ys, 2)} ({ys})')
+
+    for a,b in swaps.items():
+        gates[a], gates[b] = gates[b], gates[a]
+
+    while len(gates) > 0:
+        keys = list(gates.keys())
+        for k in keys:
+            gate = gates[k]
+            if gate[0] in inputs and gate[2] in inputs:
+                a = inputs[gate[0]]
+                b = inputs[gate[2]]
+                if gate[1] == 'AND':
+                    inputs[k] = a & b
+                elif gate[1] == 'OR':
+                    inputs[k] = a | b
+                elif gate[1] == 'XOR':
+                    inputs[k] = a ^ b
+                gates.pop(k)
+
+    zees = ''.join(map(lambda x: str(inputs[x]), reversed(sorted([k for k in inputs.keys() if k.startswith('z')]))))
+    print(f'Z={int(zees, 2)} ({zees}) should be X+Y={int(xs, 2) + int(ys, 2)}')
+
+    all_swaps = list(swaps.keys()) + list(swaps.values())
+    print(f'Swaps: {','.join(sorted(all_swaps))}')
 
 
 runIt(part1, part2)
