a064701d701f051002665124d918b1e14255f076
Tom Stamm <stammt@gmail.com>
2025-01-13 07:22:47 -0700

    2022 Day 19 part 2 and optimizations

--- aoc2022/src/main/kotlin/Day19.kt
+++ aoc2022/src/main/kotlin/Day19.kt
@@ -10,94 +10,149 @@ fun main(args: Array<String>) {
 }
 
 var stateCacheHits = 0
+
+fun day19part2(input: List<String>) {
+    val blueprints = parseBlueprints(input)
+
+    val initialState = MiningState(Resources(0, 0, 0, 0),
+        Resources(1, 0, 0, 0),
+        Resources(0, 0, 0, 0),
+        32)
+
+    var result = 0
+    for (i in 0..2) {
+        stateCache.clear()
+        maxResult = 0
+
+        val endState = countDown(initialState, blueprints[i])
+
+        println("\nblueprint ${i+1} has ${endState}\n")
+        result = if (result == 0) endState else (result * endState)
+    }
+    // 33, 10, 25 = 8250
+    println("result total $result")
+}
+
+
 fun day19part1(input: List<String>) {
     val blueprints = parseBlueprints(input)
 
     val initialState = MiningState(Resources(0, 0, 0, 0),
         Resources(1, 0, 0, 0),
+        Resources(0, 0, 0, 0),
         24)
 
     var qualityTotal = 0
-    var count = 0
     for (i in blueprints.indices) {
         stateCache.clear()
-        count = 0
+        maxResult = 0
 
         val endState = countDown(initialState, blueprints[i])
 
-        val quality = (i + 1) * endState.resources.geode
+        val quality = (i + 1) * endState
         qualityTotal += quality
-        println("\nblueprint ${i+1} has ${endState.resources.geode} (quality $quality)\n")
+        println("\nblueprint ${i+1} has ${endState} (quality $quality)\n")
     }
 
-    // 977 too low
     println("quality total $qualityTotal")
 }
 
-val stateCache = mutableMapOf<Pair<MiningState, Blueprint>, MiningState>()
-fun countDown(state: MiningState, blueprint: Blueprint): MiningState {
+val stateCache = mutableMapOf<MiningState, Int>()
+val buildAnything = Resources(0, 0, 0, 0)
+var maxResult = 0
+
+fun countDown(state: MiningState, blueprint: Blueprint): Int {
 //    println("time remaining ${state.timeRemaining}")
-    if (stateCache.containsKey(state to blueprint)) {
+    if (stateCache.containsKey(state)) {
 //        println("*** state cache hit for $state")
         stateCacheHits++
-        return stateCache[state to blueprint]!!
+        return stateCache[state]!!
     }
+    if (state.timeRemaining == 1) {
+        // Last minute, just mine, no more robots
+        val result = state.resources.add(state.robots).geode
+        maxResult = max(result, maxResult)
+        return result
+    }
+
+    // It's hopeless :(
+    if (!canBeatResult(maxResult, state)) {
+        return 0
+    }
+
     // what can we do with our resources?
-    val nextStates = mutableListOf<MiningState>()
+    var result = -1
 
-    // option 1: do nothing, just collect more resources
-    nextStates.add(MiningState(state.resources.add(state.robots), state.robots, state.timeRemaining - 1))
-
-    // option 2: see what we can build
-    if (state.robots.ore < blueprint.maxRobots.ore && state.resources.canBuild(blueprint.oreRobot)) {
-        val nextResources = state.resources.remove(blueprint.oreRobot).add(state.robots)
-        nextStates.add(MiningState(nextResources,
-            state.robots.add(Resources(1, 0, 0, 0)),
-            state.timeRemaining - 1))
-    }
-    if (state.robots.clay < blueprint.maxRobots.clay && state.resources.canBuild(blueprint.clayRobot)) {
-        val nextResources = state.resources.remove(blueprint.clayRobot).add(state.robots)
-        nextStates.add(MiningState(nextResources,
-            state.robots.add(Resources(0, 1, 0, 0)),
-            state.timeRemaining - 1))
-    }
-    if (state.robots.obsidian < blueprint.maxRobots.obsidian && state.resources.canBuild(blueprint.obsidianRobot)) {
-        val nextResources = state.resources.remove(blueprint.obsidianRobot).add(state.robots)
-        nextStates.add(MiningState(nextResources,
-            state.robots.add(Resources(0, 0, 1, 0)),
-            state.timeRemaining - 1))
-    }
+    // option 2: see what we can build, prioritizing geode robots
     if (state.resources.canBuild(blueprint.geodeRobot)) {
         val nextResources = state.resources.remove(blueprint.geodeRobot).add(state.robots)
-        nextStates.add(MiningState(nextResources,
-            state.robots.add(Resources(0, 0, 0, 1)),
-            state.timeRemaining - 1))
+        result  = max(result, countDown(MiningState(nextResources,
+            state.robots.add(0, 0, 0, 1),
+            buildAnything,
+            state.timeRemaining - 1), blueprint))
+    } else {
+        var canBuildObsidian = 0
+        if (state.canBuild.obsidian == 0 && state.robots.obsidian < blueprint.maxRobots.obsidian && state.resources.canBuild(blueprint.obsidianRobot)) {
+            canBuildObsidian = 1
+            val nextResources = state.resources.remove(blueprint.obsidianRobot).add(state.robots)
+            result = max(result, countDown(MiningState(nextResources,
+                state.robots.add(0, 0, 1, 0),
+                buildAnything,
+                state.timeRemaining - 1), blueprint))
+        }
+        var canBuildClay = 0
+        if (state.canBuild.clay == 0 && state.robots.clay < blueprint.maxRobots.clay && state.resources.canBuild(blueprint.clayRobot)) {
+            canBuildClay = 1
+            val nextResources = state.resources.remove(blueprint.clayRobot).add(state.robots)
+            result = max(result, countDown(MiningState(nextResources,
+                state.robots.add(0, 1, 0, 0),
+                buildAnything,
+                state.timeRemaining - 1), blueprint))
+        }
+        var canBuildOre = 0
+        if (state.canBuild.ore == 0 && state.robots.ore < blueprint.maxRobots.ore && state.resources.canBuild(blueprint.oreRobot)) {
+            canBuildOre = 1
+            val nextResources = state.resources.remove(blueprint.oreRobot).add(state.robots)
+            result = max(result, countDown(MiningState(nextResources,
+                state.robots.add(1, 0, 0, 0),
+                buildAnything,
+                state.timeRemaining - 1), blueprint))
+        }
+
+        // do nothing, just collect more resources
+        result = max(result, countDown(MiningState(state.resources.add(state.robots), state.robots,
+            Resources(canBuildOre, canBuildClay, canBuildObsidian, 0),
+            state.timeRemaining - 1), blueprint))
+
     }
 
-    if (state.timeRemaining == 1) {
-        return maxGeodes(nextStates)
-    }
-
-    val endStates = mutableListOf<MiningState>()
-    for (branch in nextStates) {
-        endStates.add(countDown(branch, blueprint))
-    }
-    val result = maxGeodes(endStates)
-
-    stateCache[state to blueprint] = result
+    stateCache[state] = result
 //    println("${state.timeRemaining} : ${blueprint.id} : $state produced ${nextStates.size} states: $nextStates")
     return result
 }
 
-fun maxGeodes(states: List<MiningState>): MiningState {
-    return states.maxByOrNull { (T) -> T.geode }!!
+// Is it possible to beat this result?
+fun canBeatResult(result: Int, state: MiningState): Boolean {
+    return state.resources.geode + maxFutureGeodes(state) >= result
 }
 
-data class MiningState(val resources: Resources, val robots: Resources, val timeRemaining: Int)
+// Max number of geodes we could mine even if we build a new geode bot every turn
+fun maxFutureGeodes(state: MiningState): Int {
+    return (state.timeRemaining * state.robots.geode) + (state.timeRemaining * ((state.timeRemaining - 1) / 2))
+}
+
+data class MiningState(val resources: Resources, // Current resources
+                       val robots: Resources, // Current robots per resource
+                       val canBuild: Resources, // Track what could have been built but was skipped, so we don't just do it later to get the same result
+                       val timeRemaining: Int)
 
 data class Resources(val ore: Int, val clay: Int, val obsidian: Int, val geode: Int) {
     fun add(other: Resources): Resources {
         return Resources(ore + other.ore, clay + other.clay, obsidian + other.obsidian, geode + other.geode)
+    }
+
+    fun add(ore1: Int, clay1: Int, obsidian1: Int, geode1: Int): Resources {
+        return Resources(ore + ore1, clay + clay1, obsidian + obsidian1, geode + geode1)
     }
 
     fun remove(other: Resources): Resources {
