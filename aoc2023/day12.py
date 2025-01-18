1930a2b2ac219fa82bbc58b4a491cf1873061582
Tom Stamm <stammt@gmail.com>
2025-01-18 12:58:52 -0700

    2023 Day 12 part 2, gave up...

--- aoc2023/day12.py
+++ aoc2023/day12.py
@@ -1,4 +1,5 @@
 import functools
+from typing import Tuple
 from aoc_utils import runIt, PuzzleInput, split_ints
 import math
 import re
@@ -12,7 +13,7 @@ testInput = """???.### 1,1,3
 ?###???????? 3,2,1"""
 input = PuzzleInput('input-day12.txt', testInput)
 
-lines = input.getInputLines(test=True)
+lines = input.getInputLines(test=False)
 
 def apply(s, pattern) -> str:
     option = ''
@@ -114,6 +115,31 @@ def part1():
 
     print(f'Total: {result}')
 
+
+### Copied from Norvig's python notebook after struggling with this for HOURS.
+### Remember to go for the simplest solution, if it asks for a count, just
+### figure out the count. Don't try all combinations!
+# Still don't fully understand why caching is so effective here...
+@functools.cache
+def arrangements(springs: str, runs: Tuple[int]) -> int:
+    """Count the number of arranagements of `springs` that is consistent with `runs`."""
+    if not runs:
+        return (0 if '#' in springs else 1) # One arrangement with no springs / no runs
+    elif possible_damage(springs) < sum(runs):
+        return 0 # Not enough damaged springs left to match runs
+    else: 
+        R, rest = runs[0], runs[1:]
+        # Consider the case where first R characters in springs are damaged
+        damaged = (0 if (possible_damage(springs[:R]) != R or springs[R:].startswith('#'))
+                   else arrangements(springs[R + 1:], rest))
+        # Consider the case where first character in springs is undamaged
+        undamaged = (0 if springs[0] == '#' else arrangements(springs[1:], runs))
+        return damaged + undamaged
+
+def possible_damage(springs: str) -> int: 
+    """The number of damaged springs ('#') plus possibly damaged springs ('?')."""
+    return springs.count('#') + springs.count('?')
+
 def part2():
     result = 0
     for line in lines:
@@ -124,7 +150,8 @@ def part2():
         # print(f'{x5numberPattern}')
         numbers = split_ints(numberPattern, ',')
 
-        lineOptions = blockDots(pattern, numbers)
+        # lineOptions = blockDots(pattern, numbers)
+        lineOptions = arrangements(pattern, tuple(numbers))
 
         print(f'{lineOptions} : {pattern1} - {numberPattern1} \n{pattern} {numbers}')
         print(lineOptions)
