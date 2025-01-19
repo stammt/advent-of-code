85b20cb8b1d587a84928124bd6f656ce3684f076
Tom Stamm <stammt@gmail.com>
2025-01-18 17:32:53 -0700

    2023 day 13 cleanup part 1

--- aoc2023/day13.py
+++ aoc2023/day13.py
@@ -1,4 +1,4 @@
-import aoc_utils
+from aoc_utils import PuzzleInput, runIt
 import math
 import re
 import itertools
@@ -18,11 +18,11 @@ testInput = """#.##..##.
 #####.##.
 ..##..###
 #....#..#"""
-input = aoc_utils.PuzzleInput('input-day13.txt', testInput)
+input = PuzzleInput('input-day13.txt', testInput)
 
 lines = input.getInputLines(test=False)
 
-def part1(lines):
+def parsePatterns():
     patterns = []
     pattern = []
     for line in lines:
@@ -32,56 +32,69 @@ def part1(lines):
         else:
             pattern.append(line)
     patterns.append(pattern)
+    return patterns
 
-    print(f'Found {len(patterns)} patterns')
+def findHorizontalReflection(p) -> int:
+    # look for reflection across a horizontal line
+    for i in range(len(p) - 1):
+        if p[i] == p[i+1]:
+            # see if the reflection extends in both directions
+            above = i-1
+            below = i+2
+            found = True
+            while above >= 0 and below < len(p):
+                if (p[above] != p[below]):
+                    found = False
+                    break
+                above -= 1
+                below += 1
+            if found:
+                return i + 1
+    return -1
+
+def findVerticalReflection(p) -> int:
+    # look for reflection across a vertical line
+    for i in range(len(p[0]) - 1):
+        col1 = [line[i] for line in p]
+        col2 = [line[i+1] for line in p]
+        
+        if col1 == col2:
+            # see if the reflection extends in both directions
+            left = i-1
+            right = i+2
+            found = True
+            while left >= 0 and right < len(p[0]):
+                col1 = [line[left] for line in p]
+                col2 = [line[right] for line in p]
+                if (col1 != col2):
+                    found = False
+                    break
+                left -= 1
+                right += 1
+            if found:
+                return i + 1
+                break
+    return -1
+
+
+def part1():
+    patterns = parsePatterns()
 
     horizontalCount = 0
-    for p in patterns:
-        # look for reflection across a horizontal line
-        for i in range(len(p) - 1):
-            if p[i] == p[i+1]:
-                # see if the reflection extends in both directions
-                above = i-1
-                below = i+2
-                found = True
-                while above >= 0 and below < len(p):
-                    if (p[above] != p[below]):
-                        found = False
-                        break
-                    above -= 1
-                    below += 1
-                if found:
-                    print(f'Found line between {i} and {i+1} in:\n{p}')
-                    horizontalCount += i + 1
-                    break
-
     verticalCount = 0
     for p in patterns:
-        # look for reflection across a vertical line
-        for i in range(len(p[0]) - 1):
-            col1 = [line[i] for line in p]
-            col2 = [line[i+1] for line in p]
-            
-            if col1 == col2:
-                print(f'Cols {i} and {i+1} are the same {col1} -- {col2}')
-                # see if the reflection extends in both directions
-                left = i-1
-                right = i+2
-                found = True
-                while left >= 0 and right < len(p[0]):
-                    col1 = [line[left] for line in p]
-                    col2 = [line[right] for line in p]
-                    if (col1 != col2):
-                        found = False
-                        break
-                    print(f'-- Cols {left} and {right} are the same {col1} -- {col2}')
-                    left -= 1
-                    right += 1
-                if found:
-                    print(f'Found vertical line between {i} and {i+1} in:\n{p}')
-                    verticalCount += i + 1
-                    break
+        h = findHorizontalReflection(p)
+        if h != -1:
+            horizontalCount += h
+        else:
+            v = findVerticalReflection(p)
+            if v != -1:
+                verticalCount += v
+
     total = verticalCount + (100 * horizontalCount)
     print(f'Total {total} : {verticalCount} vertical lines, {horizontalCount} horizontal lines')
 
-part1(lines)
+def part2():
+    print('nyi')
+
+runIt(part1, part2)
