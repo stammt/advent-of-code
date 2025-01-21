f7c8031ef57701f7db8af9e5d5090de88671ed68
Tom Stamm <stammt@gmail.com>
2025-01-21 07:43:50 -0700

    2023 day 19 part 2 works on sample, not real data...

--- aoc2023/day19.py
+++ aoc2023/day19.py
@@ -1,3 +1,4 @@
+from operator import mul
 from aoc_utils import PuzzleInput, runIt
 import functools
 import math
@@ -26,10 +27,9 @@ input = PuzzleInput('input-day19.txt', testInput)
 
 lines = input.getInputLines(test=False)
 
-def part1():
-    sections = [list(p) for _,p in groupby(lines, key=lambda x: x != '') if p]
+def parse_workflows(workflow_section) -> dict:
     workflows = dict()
-    for line in sections[0]:
+    for line in workflow_section:
         name = line[0:line.find('{')]
         ruleString = line[len(name)+1:-1]
         rules = [] # list of tuples: (attr, </>, value, destination) or just destination for the fallback
@@ -40,38 +40,37 @@ def part1():
             else:
                 rules.append((r[0], r[1], int(r[2:colon]), r[colon+1:]))
         workflows[name] = rules
+    return workflows
 
+def parse_parts(parts_section) -> list:
     parts = []
-    for line in sections[2]:
+    for line in parts_section:
         values = line[1:-1].split(',')
         d = dict()
         for v in values:
             [k,n] = v.split('=')
             d[k] = int(n)
         parts.append(d)
-    
-    # print(workflows)
-    # print(parts)
+    return parts
+
+def part1():
+    sections = [list(p) for _,p in groupby(lines, key=lambda x: x != '') if p]
+    workflows = parse_workflows(sections[0])
+    parts = parse_parts(sections[2])
 
     accepted = []
     rejected = []
     for part in parts:
-        # print(f'\nPart: {part}')
         ruleName = 'in'
         while ruleName != 'A' and ruleName != 'R':
             flow = workflows[ruleName]
-            # print(f'Evaluating workflow {ruleName} : {flow}')
             for rule in flow:
-                # print(f'rule: {rule}')
                 if len(rule) == 1:
-                    # print(f' falling back to {rule[0]}')
                     ruleName = rule[0]
                     break
                 v1 = part[rule[0]]
                 v2 = rule[2]
-                # print(f'v1={v1}, v2={v2}, op={rule[1]}')
                 if (rule[1] == '>' and v1 > v2) or (rule[1] == '<' and v1 < v2):
-                    # print(f' passed rule {rule}, next is {rule[3]}')
                     ruleName = rule[3]
                     break
         if ruleName == 'A':
@@ -86,7 +85,62 @@ def part1():
     total = sum([p['x'] + p['m'] + p['a'] + p['s'] for p in accepted])
     print(f'Total: {total}')
 
+def solve(workflow_name: str, ranges: dict[str, range], workflows) -> int:
+    # for each rule, recurse, with the ranges restricted for that rule
+    # if we hit 'A', return the final range
+    # if multiple paths return from 'A', merge the ranges - how to deal with gaps?
+    if workflow_name == 'A':
+        print(f'Reached A  with {ranges}')
+        return math.prod(len(c) for c in [ranges[k] for k in ['x', 'm', 'a', 's']])
+    elif workflow_name == 'R':
+        return 0
+
+    count = 0
+
+    workflow = workflows[workflow_name]
+    # print(f'Evaluating workflow {workflow_name} : {workflow} : {ranges}')
+    updated = dict(ranges)
+    for rule in workflow:            
+        if len(rule) == 1:
+            # fallback rule
+            count += solve(rule[0], updated, workflows)
+        else:
+            a = rule[0]
+            r = ranges[a]
+            op = rule[1]
+            b = rule[2]
+
+            r_inverse = r
+            if op == '>':
+                # remove a <= b from the range
+                r_inverse = range(r.start, b+1)
+                r = range(b + 1, r.stop)
+            elif op == '<':
+                # remove a >= b from the range
+                r_inverse = range(b, r.stop)
+                r = range(r.start, b)
+
+            # updated = dict(ranges)
+
+            flow_rules = dict(updated)
+            flow_rules[a] = r
+            count += solve(rule[3], flow_rules, workflows)
+
+            updated[a] = r_inverse # needs to be inverse of this rule
+
+    return count
+
+
 def part2():
-    print('nyi')
+    sections = [list(p) for _,p in groupby(lines, key=lambda x: x != '') if p]
+    workflows = parse_workflows(sections[0])
+
+    # Start with 'in', range(1,4001) for each letter, find all paths to 'A' and find the union of their rules?
+    ranges = {k: range(1, 4001) for k in ['x', 'm', 'a', 's']}
+    # start = workflows['in']
+    count = solve('in', ranges, workflows)
+
+    # 196723013358875 too high
+    print(f'Count {count}')
 
 runIt(part1, part2)
