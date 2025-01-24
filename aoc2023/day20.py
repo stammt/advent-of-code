62c8e711212105b314e024bac5969b439d1582f9
Tom Stamm <stammt@gmail.com>
2025-01-24 08:37:35 -0700

    2023 Day 22 part 2

--- aoc2023/day20.py
+++ aoc2023/day20.py
@@ -35,7 +35,7 @@ def part1():
         moduleStr, destinationsStr = line.split(' -> ')
         name = moduleStr if moduleStr == "broadcaster" else moduleStr[1:]
         type = moduleStr if moduleStr == "broadcaster" else moduleStr[:1]
-        destinations = destinationsStr.split(", ")
+        destinations = tuple(destinationsStr.split(", "))
         if name == "broadcaster":
             broadcaster = destinations
         else:
@@ -53,7 +53,7 @@ def part1():
     highPulseCount = 0
     # Queue of pulses as a tuple (high/low, sender, receiver)
     q = deque()
-    for pushes in range(1000):
+    for pushes in range(10000):
         lowPulseCount += 1 # button sends a low pulse
         q.extend([(False, 'broadcaster', m) for m in broadcaster])
 
@@ -91,8 +91,103 @@ def part1():
     print(f'{highPulseCount} high pulses * {lowPulseCount} low pulses = {lowPulseCount * highPulseCount}')
 
 def part2():
-    # Starting with the rx module, work backwards to find the sequence of
-    # pulses that would have sent it a single low pulse.
-    print('huh')
+    # From the input:
+    # &rs -> rx
+    # So we need all inputs to rs to be High so it will send a Low pulse to rx
+    # bt, dl, fw, rv need to send High
+
+    # Find when all of them have sent a High pulse without sending a Low pulse afterwards
+
+    broadcaster = []
+    # dict of module name -> tuple of type, state, destinations
+    modules: dict[str:tuple(str, dict[str, bool], list[str])] = {}
+    conjnction_inputs = defaultdict(list)
+    for line in lines:
+        moduleStr, destinationsStr = line.split(' -> ')
+        name = moduleStr if moduleStr == "broadcaster" else moduleStr[1:]
+        type = moduleStr if moduleStr == "broadcaster" else moduleStr[:1]
+        destinations = tuple(destinationsStr.split(", "))
+        if name == "broadcaster":
+            broadcaster = destinations
+            modules[name] = Module('broadcaster', defaultdict(bool), destinations)
+        else:
+            modules[name] = Module(type, defaultdict(bool), destinations)
+        for d in destinations:
+            conjnction_inputs[d].append(name)
+
+    # Initialize the inputs of the conjunctions to False
+    for name in conjnction_inputs.keys():
+        if name in modules and modules[name][0] == CONJUNCTION:
+            for i in conjnction_inputs[name]:
+                modules[name][1][i] = False
+
+
+    # # walk back from rx to broadcaster
+    # dest = deque( [('rx', 0)])
+    # seen = set()
+    # while dest:
+    #     d, level = dest.popleft()
+    #     for k,v in modules.items():
+    #         if d in v.outputs:
+    #             if (k,d) not in seen:
+    #                 print(f'{'..'* level} {v.type}{k} goes to {d}')
+    #                 dest.append((k, level + 1))
+    #                 seen.add((k,d))
+
+
+
+    # Queue of pulses as a tuple (high/low, sender, receiver)
+    q = deque()
+    pushes = 0
+    while pushes < 100000:
+        pushes += 1
+        q.extend([(False, 'broadcaster', m) for m in broadcaster])
+
+        # print(f'bt: {modules['bt']}')
+        # print(f'dl: {modules['dl']}')
+        # print(f'fr: {modules['fr']}')
+        # print(f'rv: {modules['rv']}')
+        if any(modules['rs'].state.values()):
+            print(f'{pushes} rs: {modules['rs']}')
+
+        print(f'{pushes} {[modules[x].state[STATE] for x in broadcaster]}')
+
+
+        while q:
+            pulse = q.popleft()
+            # print(f'{pulse[1]} -{'high' if pulse[0] else 'low'}-> {pulse[2]}')
+
+            if pulse[2] == 'rx':
+
+                # print(f'rx: {pulse[0]} after {pushes}')
+                if pulse[0] == False:
+                    print(f'rx got a low pulse: {pushes}')
+                    return
+            if pulse[2] not in modules:
+                continue
+
+            receiver = modules[pulse[2]]
+            sendPulse = None
+
+            if receiver.type == FLIPFLOP:
+                # flip-flop
+                if pulse[0] == False:
+                    receiver.state[STATE] = not receiver.state[STATE]
+                    sendPulse = receiver[1][STATE]
+            else:
+                # conjunction
+                receiver.state[pulse[1]] = pulse[0]
+                # If any inputs are low, send a high pulse
+                # if pulse[2] == 'mj':
+                #     print(f'At push {pushes} {pulse[2]} is {receiver}')
+                sendPulse = len(receiver.state) == 0 or not all(receiver.state.values())
+                # if pulse[2] == 'rs':
+                #     print(f'At push {pushes} {pulse[2]} is {receiver} will send {sendPulse}')
+
+            if sendPulse != None:
+                for d in receiver.outputs:
+                    q.append((sendPulse, pulse[2], d))
+
+    print(f'Never got a low pulse to rx...')
 
 runIt(part1, part2)
--- aoc2023/day22.py
+++ aoc2023/day22.py
@@ -1,10 +1,10 @@
-import aoc_utils
+from aoc_utils import PuzzleInput, split_ints, runIt
 import functools
 import math
 import re
 import itertools
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 
 testInput = r"""1,0,1~1,2,1
 0,0,2~2,0,2
@@ -13,7 +13,7 @@ testInput = r"""1,0,1~1,2,1
 2,0,5~2,2,5
 0,1,6~2,1,6
 1,1,8~1,1,9"""
-input = aoc_utils.PuzzleInput('input/input-day22.txt', testInput)
+input = PuzzleInput('input-day22.txt', testInput)
 
 lines = input.getInputLines(test=False)
 
@@ -22,30 +22,18 @@ class Brick:
         self.start = start
         self.end = end
         self.name = name
+        self.bottom_points = {(x,y) for x in range(self.start[0], self.end[0] + 1) for y in range(self.start[1], self.end[1] + 1)}
 
     def fallBy(self, dz):
         self.start[2] -= dz
         self.end[2] -= dz
-
-    def getBottomPoints(self) -> list[(int, int)]:
-        points = []
-        if (self.start[0] > self.end[0]):
-            print(f'x is backwards! {self}')
-        if (self.start[1] > self.end[1]):
-            print(f'y is backwards! {self}')
-        for x in range(self.start[0], self.end[0] + 1):
-            for y in range(self.start[1], self.end[1] + 1):
-                points.append((x, y))
-        return points
  
+    def overlapsXY(self, brick) -> bool:
+        return not self.bottom_points.isdisjoint(brick.bottom_points)
+    
     def coversXY(self, x, y) -> bool:
-        return (x, y) in self.getBottomPoints()
-    
-    def getZAtPoint(self, x, y) -> int|None:
-        if not self.coversXY(x, y):
-            return None
-        return self.getHighestZ()
-    
+        return (x, y) in self.bottom_points
+        
     def getHighestZ(self) -> int:
         return max(self.start[2], self.end[2])
     
@@ -60,8 +48,8 @@ def parseBricks(lines) -> list[Brick]:
     name = 'A'
     for line in lines:
         start, end = line.split('~')
-        start = aoc_utils.splitInts(start, ',')
-        end = aoc_utils.splitInts(end, ',')
+        start = split_ints(start, ',')
+        end = split_ints(end, ',')
         bricks.append(Brick(start, end, name))
         name = chr(ord(name) + 1)
     return bricks
@@ -72,21 +60,17 @@ def bricksToString(bricks):
         s.append(str(b))
     return ''.join(s)
 
-def howManyWouldFall(bricks, supporting):
-    # Find bricks that only these bricks support
-    s = set(filter(lambda x: (bricks == supporting[x]), supporting))
-    fallingCount = len(s)
+def howManyWouldFall(disintegrated: set[Brick], supporting):
+    # Find bricks only supported by these bricks
+    s = list(filter(lambda x: x not in disintegrated and disintegrated.issuperset(supporting[x]), supporting))
 
-    # Recursively see which bricks are only supported by any combination of the ones that fell next
+    # Recursively see which bricks are only supported by any combination of the ones that have already
+    # disintegrated, stopping when no more fall.
+    disintegrated.update(s)
     if len(s) > 0:
-        for l in range(len(s) + 1):
-            for subset in itertools.combinations(s, l):
-                fallingCount += howManyWouldFall(set(subset), supporting)
-        # fallingCount += howManyWouldFall(s, supporting)
+        howManyWouldFall(disintegrated, supporting)
 
-    return fallingCount
-
-def part1(lines):
+def part1():
     bricks = parseBricks(lines)
 
     # translate the bricks down to have them fall on the ground
@@ -95,33 +79,22 @@ def part1(lines):
     # bricks that have landed (on the ground or on other bricks)
     landed = []
 
-    # map of brick -> [supporting bricks]
+    # map of brick -> set[supporting bricks]
     supporting = {}
 
     # sort the falling bricks by their z index so we land the lowest
     # bricks first
     fallingBricks = sorted(fallingBricks, key=lambda b: min(b.start[2], b.end[2]))
 
-    highestZAtPoint = {}
-    highestBrickAtPoint = {}
+    highestZAtPoint = defaultdict(int)
     for brick in fallingBricks:
         # find the highest point of landed bricks for any part of
         # this brick. The bricks containing those points are supporting
         # this one when it lands.
-        pointsToCheck = brick.getBottomPoints()
-        highest = 0
-        highestBricks = set()
-        # print(f'Checking brick {b} : {pointsToCheck}')
-        for p in pointsToCheck:
-            pz = highestZAtPoint[p] if p in highestZAtPoint else 0
-            if pz == highest:
-                if pz > 0:
-                    highestBricks.add(highestBrickAtPoint[p])
-            elif pz > highest:
-                highest = pz
-                highestBricks = set()
-                if pz > 0:
-                    highestBricks.add(highestBrickAtPoint[p])
+        highest = max(highestZAtPoint[p] for p in brick.bottom_points)
+
+        # bricks that are at the highest z and overlap this brick's x,y will be supporting this brick
+        highestBricks = set(filter(lambda b: b.getHighestZ() == highest and b.overlapsXY(brick), landed))
 
         # drop this brick by enough that it's lowest point will be
         # one higher than the highest point.
@@ -135,39 +108,34 @@ def part1(lines):
 
         # update the highest points with this brick
         z = brick.getHighestZ()
-        for p in pointsToCheck:
+        for p in brick.bottom_points:
             highestZAtPoint[p] = z
-            highestBrickAtPoint[p] = brick
 
-    ## Part 1
-    # safe = 0
-    # for b in landed:
-    #     # Find bricks that only this brick supports
-    #     s = list(filter(lambda x: (b in supporting[x] and len(supporting[x])) == 1, supporting))
-    #     if len(s) == 0:
-    #         safe += 1
+    # Part 1
+    safe = 0
+    for b in landed:
+        # Find bricks that only this brick supports
+        s = list(filter(lambda x: (b in supporting[x] and len(supporting[x])) == 1, supporting))
+        if len(s) == 0:
+            safe += 1
 
-    # print(f'Safe to disintigrate {safe} bricks')
+    print(f'Safe to disintigrate {safe} bricks')
 
     ## Part 2
     total = 0
-    safe = 0
     for b in landed:
-        sb = set()
-        sb.add(b)
-        fallingCount = howManyWouldFall(sb, supporting)
-        print(f'{fallingCount} would fall for {b}')
-        if fallingCount == 0:
-            safe += 1
-        else:
-            total += fallingCount
-
-    print(f'{safe} safe to remove')
+        disintegrated = set()
+        disintegrated.add(b)
+        howManyWouldFall(disintegrated, supporting)
+        fallingCount = len(disintegrated) - 1
+        # print(f'{fallingCount} would fall for {b}')
+        total += fallingCount
     
+    #41610
     print(f'{total} would fall')
-    #2086 too low
-    #11351 too low
+
+def part2():
+    print('nyi')
 
 
-
-part1(lines)
+runIt(part1, part2)
