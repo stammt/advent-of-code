40c085421698eb7ee9d07043280e378fe86c6608
Tom Stamm <stammt@gmail.com>
2025-02-10 12:39:46 -0700

    2019 day 10 part 2

---
+++ aoc2019/README.md
@@ -0,0 +1,8 @@
+2019
+
+The intcode puzzles have been pretty straightforward so far, but it's been fun to have a solution that builds on previous solutions instead of starting fresh each time.
+
+Day 10:
+
+- This one was a little tricky, I started with a brute-force approach of looping through the asteroids; then for each one, find the line from it to each other asteroid, and loop through them _again_ to find any that are on the line, between the two asteroids, and blocking the view. Later I realized that I just had to identify each line by the angle from the asteroid being considered. If multiple asteroids are on a line with that angle, it doesn't matter, it just counts once! I'm pretty rusty on trig so ended up using numpy.arctan2 and flipping the map so that I could compare the angle of the line to the x axis.
+- For Part 2, it was a simple update to store the list of asteroids on each angle, and then loop through them and destroy the closest one at each iteration. I got mixed up a bit with how I "flipped" the graph, and had to go counter-clockwise on the flipped version and translate back to the original version, but it worked out!
--- aoc2019/src/python/day10.py
+++ aoc2019/src/python/day10.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from aoc_utils import PuzzleInput, runIt, Point, manhattan_distance
 from itertools import count
 from numpy import arctan, arctan2, pi
@@ -28,8 +29,8 @@ lines = input.getInputLines(test=False)
 
 
 def part1():
-    # Normalize the y values so they go "up" from zero
-    asteroids = {(x, len(lines) - y) for y in range(len(lines)) for x in range(len(lines[0])) if lines[y][x] == '#'}
+    # Flip the y values so they go "up" from zero
+    asteroids = {(x, len(lines) - 1 - y) for y in range(len(lines)) for x in range(len(lines[0])) if lines[y][x] == '#'}
 
     # for each asteroid, find the angles to every other asteroid and track the unique values. (if multiple other
     # asteroids are at the same angle, we only see the closest one)
@@ -42,42 +43,64 @@ def part1():
             x2, y2 = other
             if other == asteroid: continue
 
-            # subtract the asteroid's x1,y1 to normalize to 0,0 origin
+            # subtract the asteroid's x1,y1 to normalize to 0,0 origin for calling arctan2
             angle1 = arctan2([y2-y1], [x2-x1]) * (180 / pi)
             angles.add(angle1[0])
             
-        # print(f'Found {count} vs {len(angles)} angles')
         if len(angles) > best_count:
             best_count = len(angles)
-            best = asteroid
+            best = (asteroid[0], len(lines) - 1 - asteroid[1])
 
     print(f'Best is {best} with a view of {best_count}')
 
 
 def part2():
-    print('nyi')
+    # Flip the y values so they go "up" from zero
+    asteroids = {(x, len(lines) - 1 - y) for y in range(len(lines)) for x in range(len(lines[0])) if lines[y][x] == '#'}
 
-def angles():
-    for asteroid in asteroids:
-        x1, y1 = asteroid
-        angles = set()
-        for other in asteroids:
-            if other == asteroid: continue
+    # Origin is 20,18 from part 1, adjusted for the flipped view
+    origin = (20, len(lines) - 1 - 18) # Test: (11, len(lines) - 1 - 13)
+    
+    # build a list of asteroids for each angle from the "origin"
+    angles = defaultdict(list)
+    x1, y1 = origin
+    for other in asteroids:
+        x2, y2 = other
+        if other == origin: continue
 
-            x2, y2 = other
+        # subtract the origin's x1,y1 to normalize to 0,0 origin
+        # Then add 270 so that an angle of 0 is down, which is "up" in the flipped view.
+        angle1 = (arctan2([y2-y1], [x2-x1]) * (180 / pi)) + 270
+        a = angle1[0]
+        if a < 0:
+            a = (360 + a) % 360
+        angles[a % 360].append(other)
 
-            if (x1 == x2):
-                angles.add(0 if y1 < y2 else 180)
-            elif (y1 == y2):
-                angles.add(90 if x1 < x2 else 270)
-            else:
-                slope = (y2 - y1) / (x2 - x1)
-                angle = round((arctan(slope) * (180 / math.pi)) + 90, 4)
-                print(f'adding angle {angle} from {asteroid} to {other}')
-                angles.add(angle)
-            
-        if len(angles) > best_count:
-            best_count = len(angles)
-            best = asteroid
+    # sort each list by distance from the origin
+    for l in angles.values():
+        l.sort(key = lambda x: manhattan_distance(origin, x))
+
+    # sort the list of angles descending, since they are going counter-clockwise in the flipped world.
+    angle_list = list(angles.keys())
+    angle_list.sort(reverse=True)
+
+    # Special case move angle 0 to the beginning of the list
+    angle_list.remove(0)
+    angle_list.insert(0, 0)
+
+    # Loop through the angles and pop the closest asteroid from each list until we hit 200
+    a = 0 # which angle
+    i = 0 # how many asteroids were destroyed so far
+    while True:
+        l = angles[angle_list[a]]
+        if len(l) > 0:
+            popped = l.pop(0)
+            i += 1
+            if i == 200:
+                adjusted = (popped[0], len(lines) - 1 - popped[1])
+                print(f'200th is {adjusted} : value is {(adjusted[0] * 100) + adjusted[1]}')
+                break
+        a = (a + 1) % len(angle_list)
+
 
 runIt(part1, part2)
