d6479b330ff7be352f9ece644364b75641703cf9
Tom Stamm <stammt@gmail.com>
2025-02-13 12:31:55 -0700

    2019 day 13 added an animation for part 2 :)

--- aoc2019/src/python/day13.py
+++ aoc2019/src/python/day13.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import os
+import time
 from aoc_utils import PuzzleInput, runIt, splitInts, North, South, East, West, turn, add
 from itertools import count
 from intcode import STATE_HALTED, Intcode
@@ -14,28 +16,27 @@ TILE_PADDLE = 3
 TILE_BALL = 4
 
 # Returns (tiles, score)
-def read_output(output: list[int]) -> tuple[dict, int]:
+def read_output(output: list[int], score = 0) -> tuple[dict, int]:
     tiles = dict()
-    score = -1
+    # score = -1
     i = 0
     while i < len(output):
         x = output[i]
         y = output[i+1]
         tile = output[i+2]
         if x == -1 and y == 0:
-            print(f'score in tiles {tile}')
+            # print(f'score in tiles {tile}')
             score = tile
         else:
             tiles[(x, y)] = tile
         i += 3
-    # print(f'Output: {output}')
-    # print(f'Tiles: {tiles}')
     return (tiles, score)
 
 def find_tiles(t, tiles):
     return [p for p in tiles.keys() if tiles[p] == t]
 
-def print_tiles(tiles):
+def print_tiles(tiles, score):
+    os.system('clear')
     maxx = max([p[0] for p in tiles.keys()])
     maxy = max([p[1] for p in tiles.keys()])
     for y in range(maxy + 1):
@@ -43,16 +44,20 @@ def print_tiles(tiles):
         for x in range(maxx + 1):
             t = tiles[(x,y)]
             if t == TILE_WALL:
-                s += '#'
+                s += '\033[96m#'
             elif t == TILE_BLOCK:
-                s += '$'
+                s += '\033[92mX'
             elif t == TILE_BALL:
-                s += 'o'
+                s += '\033[91mo'
             elif t == TILE_PADDLE:
-                s += '_'
+                s += '\033[91m_'
             else:
                 s += ' '
         print(s)
+
+    print()
+    print(score)
+    time.sleep(0.02)
 
 def part1():
     computer = Intcode(lines[0])
@@ -67,7 +72,7 @@ def part2():
     computer.memory[0] = 2 # free play!
     computer.run_with_input([0])
     tiles, score = read_output(computer.output)
-    print_tiles(tiles)
+    # print_tiles(tiles)
     
     ball = find_tiles(TILE_BALL, tiles)[0]
     old_ball = None
@@ -77,24 +82,23 @@ def part2():
 
     
     while len(blocks) > 0:
+        print_tiles(tiles, score)
         move = 0
         if old_ball != None:
+            # Move right if the ball is to our right and moving right, left if the ball is moving left, otherwise sit still
             move = 1 if ball[0] > old_ball[0] and paddle[0] < ball[0] else -1 if ball[0] < old_ball[0] and paddle[0] > ball[0] else 0
-        # print(f'Moving {move}')
         computer.run_with_input([move])
-        tiles_diff, score = read_output(computer.output)
+        tiles_diff, score = read_output(computer.output, score)
         if computer.state == STATE_HALTED:
             print('GAME OVER MAN!')
             break
 
-        # print(tiles_diff)
         tiles.update(tiles_diff)
         
         blocks = find_tiles(TILE_BLOCK, tiles)
         old_ball = ball
         ball = find_tiles(TILE_BALL, tiles_diff)[0]
         paddle = find_tiles(TILE_PADDLE, tiles)[0]
-        # print(f'Ball: {ball}, Paddle: {paddle}, blocks: {len(blocks)}')
     
 
 runIt(part1, part2)
