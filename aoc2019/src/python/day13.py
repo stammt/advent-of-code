c0db3de64ae2f9de9ca55c0f638c0306eac38e90
Tom Stamm <stammt@gmail.com>
2025-02-13 06:44:43 -0700

    2019 day 13 start part 2

--- aoc2019/src/python/day13.py
+++ aoc2019/src/python/day13.py
@@ -13,23 +13,38 @@ TILE_BLOCK = 2
 TILE_PADDLE = 3
 TILE_BALL = 4
 
-def part1():
-    computer = Intcode(lines[0])
+# Returns (tiles, score)
+def read_output(output: list[int]) -> tuple[dict[int], int]:
     tiles = defaultdict(int)
-    computer.run_with_input([])
+    score = 0
     i = 0
-    while i < len(computer.output):
-        x = computer.output[i]
-        y = computer.output[i+1]
-        tile = computer.output[i+2]
-        if tile != 0:
+    while i < len(output):
+        x = output[i]
+        y = output[i+1]
+        tile = output[i+2]
+        if x == -1 and y == 0:
+            print(f'score {tile}')
+            score = tile
+        elif tile != 0:
             tiles[(x, y)] = tile
         i += 3
+    return (tiles, score)
+
+def part1():
+    computer = Intcode(lines[0])
+    computer.run_with_input([])
+    tiles, score = read_output(computer.output)
     
     blocks = len(list(filter(lambda t: t == TILE_BLOCK, tiles.values())))
     print(blocks)
 
 def part2():
-    print('nyi')
+    computer = Intcode(lines[0])
+    computer.memory[0] = 2 # free play!
+    computer.run_with_input([0])
+    tiles, score = read_output(computer.output)
+    
+    print(score)
+    
 
 runIt(part1, part2)
