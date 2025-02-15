1c82253c12d714d9e91320d20bfe5d932b394ad6
Tom Stamm <stammt@gmail.com>
2025-02-15 17:11:28 -0500

    2019 day 15 part 2

--- aoc2019/src/python/day15.py
+++ aoc2019/src/python/day15.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import os
 import time
-from aoc_utils import A_star, Grid, PuzzleInput, manhattan_distance, runIt, splitInts, North, South, East, West, turn, add
+from aoc_utils import A_star, Grid, PuzzleInput, manhattan_distance, runIt, splitInts, North, South, East, West, turn, add, cardinal_directions
 from itertools import count
 from intcode import STATE_HALTED, Intcode
 
@@ -40,6 +40,7 @@ def explore(pos, path, grid, computer):
     explore_dir(epos, 4, 3, path, grid, computer)    
 
 def print_grid(grid):
+    os.system('clear')
     minx = min(map(lambda k: k[0], grid.keys()))
     maxx = max(map(lambda k: k[0], grid.keys()))
     miny = min(map(lambda k: k[1], grid.keys()))
@@ -54,25 +55,48 @@ def print_grid(grid):
             else:
                 s += grid[(x,y)]
         print(s)
+    time.sleep(0.02)
     
 def part1():
     computer = Intcode(lines[0])
     grid = defaultdict(int)
     start = (0, 0)
     explore(start, [], grid, computer)
-    print_grid(grid)
     grid_grid = Grid(grid)
     oxygen = grid_grid.find(GOAL)
 
     path = A_star(start, oxygen, h = lambda p: manhattan_distance(p, oxygen), grid = grid_grid)
-    print(path)
-    for p in path:
-        grid[p] = '*'
-    print_grid(grid)
     print(len(path) - 1)
 
 def part2():
     computer = Intcode(lines[0])
     
+    grid = defaultdict(int)
+    start = (0, 0)
+    explore(start, [], grid, computer)
+    grid_grid = Grid(grid)
+    oxygen = grid_grid.find(GOAL)
+
+    # do a bfs starting from the oxygen location, adding all positions from the next generation
+    # at each pass, until no more can be added.
+    visited = set()
+    q = [[oxygen]]
+    minutes = 0
+    while q:
+        generation = q.pop()
+        next_gen = []
+        for pos in generation:
+            visited.add(pos)
+            grid_grid[pos] = 'O'
+            neighbors = grid_grid.neighbors(pos, cardinal_directions)
+            for n in neighbors:
+                if n not in visited and grid_grid[n] != WALL:
+                    next_gen.append(n)
+        if len(next_gen) > 0:
+            q.append(next_gen)
+        # print_grid(grid_grid)
+            minutes += 1
+
+    print(minutes )
 
 runIt(part1, part2)
