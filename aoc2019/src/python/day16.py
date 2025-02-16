0fe3760c66204abcd8167f1875c168659e14375b
Tom Stamm <stammt@gmail.com>
2025-02-16 13:18:23 -0500

    2019 day 16 part 1

---
+++ aoc2019/src/python/day16.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+import sys
+from aoc_utils import PuzzleInput, runIt, Point, manhattan_distance
+from itertools import count
+from numpy import arctan, arctan2, pi
+import math
+
+# 01029498 after 4 phases
+testInput1 = r"""12345678"""
+
+# starts with 24176176 after 100 phases
+testInput2 = r"""80871224585914546619083218645595"""
+input = PuzzleInput('input/day16.txt', testInput2)
+
+lines = input.getInputLines(test=False)
+BASE_PATTERN = [0, 1, 0, -1]
+
+def expand_pattern(d: int):
+    expanded = []
+    for i in BASE_PATTERN:
+        for p in range(d):
+            expanded.append(BASE_PATTERN[i])
+    return expanded
+
+def apply_pattern(pattern: list[int], input_digits: list[int]):
+    s = 0
+    p = 1
+    for i in range(len(input_digits)):
+        s += (input_digits[i] * pattern[p])
+        p = (p + 1) % len(pattern)
+
+    return abs(s) % 10
+
+def part1():
+    input_digits = list(map(int, lines[0]))
+    for i in range(100):
+        output_digits = []
+
+        for d in range(len(input_digits)):
+            pattern = expand_pattern(d+1)
+            output_digits.append(apply_pattern(pattern, input_digits))
+        input_digits = output_digits
+
+    print(''.join(map(str, input_digits[0:8])))
+
+def part2():
+    print('nyi')
+
+runIt(part1, part2)
